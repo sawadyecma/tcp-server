3ffbf8b68be684638073e203e3e4505520a9e4a0
sawadyecma <snowslidez@yahoo.co.jp>
2021-12-11 17:35:57 +0900

    キャンセル処理

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -18,6 +19,10 @@ func logFatal(err error) {
 func main() {
 	fmt.Println("Start TCP Server")
 
+	// cancel
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	// tcpの接続アドレスを作成する
 	tcpAddr, err := net.ResolveTCPAddr("tcp", ":8080")
 	logFatal(err)
@@ -25,34 +30,42 @@ func main() {
 	// リスナーを作成する
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	logFatal(err)
-
-	listener.SetDeadline(time.Now().Add(time.Second * 10))
+	defer listener.Close()
 
 	fmt.Println("Start TCP Server...")
 
 	for {
-		// クライアントからのコネクション情報を受け取る
-		conn, err := listener.AcceptTCP()
-		if err != nil {
-			switch err := err.(type) {
-			case net.Error:
-				if err.Timeout() {
-					log.Println("Tcp Listener Close")
+		select {
+		case <-ctx.Done():
+			log.Println("Stopping TCP Server...")
+			return
+		default:
+			log.Println("Running TCP Server...")
+			listener.SetDeadline(time.Now().Add(time.Second * 10))
+
+			// クライアントからのコネクション情報を受け取る
+			conn, err := listener.AcceptTCP()
+			if err != nil {
+				switch err := err.(type) {
+				case net.Error:
+					if err.Timeout() {
+						log.Println("Tcp Listener Close")
+						return
+					}
+					if err.Temporary() {
+						log.Printf("Temporay Error: %s\n", err.Error())
+						return
+					}
+				default:
+					log.Println("Another Error!!!")
 					return
 				}
-				if err.Temporary() {
-					log.Printf("Temporay Error: %s\n", err.Error())
-					return
-				}
-			default:
-				log.Println("Another Error!!!")
-				return
+				logFatal(err)
 			}
-			logFatal(err)
+			// ハンドラーに接続情報を渡す
+			go handler(conn)
 		}
 
-		// ハンドラーに接続情報を渡す
-		go handler(conn)
 	}
 }
 
